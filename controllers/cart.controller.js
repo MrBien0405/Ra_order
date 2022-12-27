db539c0077514db08e52cb5b63bccf34dcfb4298
Goose9x <dangtandung99@gmail.com>
2022-12-27 17:36:30 +0700

    finish cart backend

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -1,36 +1,61 @@
 const db = require("../models/db");
-
+module.exports.viewAllCart = (req, res) => {
+  let userId = req.params.id;
+  if (userId) {
+    db.execute("SELECT * FROM tbl_cart WHERE userID2 = ?", [userId])
+      .then((data) => {
+        res.status(200).json({ cartCounter: data[0].length, data: data[0] });
+      })
+      .catch((err) => {
+        res.status(400).json({ error: err });
+      });
+  }
+};
 module.exports.createCart = (req, res) => {
-  console.log("a");
-  let id = Math.floor(Math.random() * 999999);
-  console.log(id);
-  let { userId } = req.signedCookies;
-  let productID = 93549;
-  let cartQuantity = 10;
-  console.log(productID);
-  console.log(userId);
-  db.execute("INSERT INTO tbl_cart VALUES(?,?,?,?)", [
-    id,
+  let { userId, productId, id } = req.body;
+  db.execute("SELECT * FROM tbl_cart WHERE userID2 = ? AND productID2 = ?", [
     userId,
-    productID,
-    cartQuantity,
+    productId,
   ])
     .then((data) => {
-      res.status(200).json({
-        message: "Cart one successfully",
-      });
+      if (data[0].length !== 0) {
+        db.execute(
+          "UPDATE tbl_cart SET cartQuantity = cartQuantity + 1 WHERE userID2 = ? AND productID2 = ?",
+          [userId, productId]
+        )
+          .then((data) => {
+            res.status(201).json({ message: "Update oke" });
+          })
+          .catch((err) => {
+            res.status(400).json({ error: err });
+          });
+      } else {
+        db.execute("INSERT INTO tbl_cart VALUES(?,?,?,?)", [
+          id,
+          userId,
+          productId,
+          1,
+        ])
+          .then((data) => {
+            res.status(200).json({
+              message: "Cart one successfully",
+            });
+          })
+          .catch((err) => {
+            res.status(500).json({
+              message: err,
+            });
+          });
+      }
     })
     .catch((err) => {
-      res.status(500).json({
-        message: err,
-      });
+      res.status(400).json({ Error: err });
     });
 };
 
 module.exports.deleteCartById = (req, res) => {
   let id = req.params.id;
-  console.log(id);
-  db.execute("DELETE FROM tbl_cart WHERE id=?", [id])
+  db.execute("DELETE FROM tbl_cart WHERE productID2=?", [id])
     .then((data) => {
       res.status(200).json({
         message: "Delete one succesfully",
@@ -59,5 +84,15 @@ module.exports.deleteUser = (req, res) => {
     });
 };
 module.exports.updateCart = (req, res) => {
-  console.log(req.query);
+  let { value, userId, productId } = req.body;
+  db.execute(
+    "UPDATE tbl_cart SET cartQuantity = ? WHERE userID2 = ? AND productID2 = ?",
+    [value, userId, productId]
+  )
+    .then((data) => {
+      res.status(200).json({ message: "update oke" });
+    })
+    .catch((err) => {
+      res.status(400).json({ error: err });
+    });
 };
--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const cartController = require("../controllers/cart.controller");
-
+router.get("/:id", cartController.viewAllCart);
 router.post("/", cartController.createCart);
 router.put("/", cartController.updateCart);
 router.delete("/", cartController.deleteUser);
